ee89c6668d505bf5b7a6f20a9d1c7263f83ae079
RokuFSD <emilianordx@gmail.com>
2021-12-14 19:27:57 -0300

    JSON added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,26 @@
 const selector = document.querySelectorAll('a[href="javascript:void(0)"]');
+const cardsData = document.querySelectorAll(".data__stats");
 
-const getSelected = () => {
-  return selector.forEach(element => {
-    if(element.className == 'selected'){
-      return element.id;
+
+console.log(cardsData);
+
+const changeCards = (option) => {
+  console.log(option);
+
+  for (let element of cardsData) {
+    console.log(element);
+    element.querySelector("p").innerHTML = 'maricon';
+    element.querySelector('h2').innerHTML = 'maricoa';
+  }
+}
+
+for(let boton of selector){
+  boton.addEventListener("click", () => {
+    const actual = document.querySelector(".selected");
+    if(boton.id != actual.id){
+      boton.classList.toggle('selected');
+      actual.classList.toggle('selected');
+      changeCards(boton.id);
     }
   })
 }
-
-
-const changeCards = (valor) => {
-  console.log(getSelected());
-}
-
