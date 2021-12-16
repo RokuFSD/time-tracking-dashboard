c03cd37075a61fe68a0b52bffd3008017c9b4e3d
RokuFSD <emilianordx@gmail.com>
2021-12-16 09:17:44 -0300

    working site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,160 +1,160 @@
 const data = [
   {
-    "title": "Work",
-    "timeframes": {
-      "daily": {
-        "current": 5,
-        "previous": 7
+    title: "Work",
+    timeframes: {
+      daily: {
+        current: 5,
+        previous: 7,
       },
-      "weekly": {
-        "current": 32,
-        "previous": 36
+      weekly: {
+        current: 32,
+        previous: 36,
       },
-      "monthly": {
-        "current": 103,
-        "previous": 128
-      }
-    }
+      monthly: {
+        current: 103,
+        previous: 128,
+      },
+    },
   },
   {
-    "title": "Play",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 2
+    title: "Play",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 2,
       },
-      "weekly": {
-        "current": 10,
-        "previous": 8
+      weekly: {
+        current: 10,
+        previous: 8,
       },
-      "monthly": {
-        "current": 23,
-        "previous": 29
-      }
-    }
+      monthly: {
+        current: 23,
+        previous: 29,
+      },
+    },
   },
   {
-    "title": "Study",
-    "timeframes": {
-      "daily": {
-        "current": 0,
-        "previous": 1
+    title: "Study",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
       },
-      "weekly": {
-        "current": 4,
-        "previous": 7
+      weekly: {
+        current: 4,
+        previous: 7,
       },
-      "monthly": {
-        "current": 13,
-        "previous": 19
-      }
-    }
+      monthly: {
+        current: 13,
+        previous: 19,
+      },
+    },
   },
   {
-    "title": "Exercise",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 1
+    title: "Exercise",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 1,
       },
-      "weekly": {
-        "current": 4,
-        "previous": 5
+      weekly: {
+        current: 4,
+        previous: 5,
       },
-      "monthly": {
-        "current": 11,
-        "previous": 18
-      }
-    }
+      monthly: {
+        current: 11,
+        previous: 18,
+      },
+    },
   },
   {
-    "title": "Social",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 3
+    title: "Social",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 3,
       },
-      "weekly": {
-        "current": 5,
-        "previous": 10
+      weekly: {
+        current: 5,
+        previous: 10,
       },
-      "monthly": {
-        "current": 21,
-        "previous": 23
-      }
-    }
+      monthly: {
+        current: 21,
+        previous: 23,
+      },
+    },
   },
   {
-    "title": "Self Care",
-    "timeframes": {
-      "daily": {
-        "current": 0,
-        "previous": 1
+    title: "Self Care",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
       },
-      "weekly": {
-        "current": 2,
-        "previous": 2
+      weekly: {
+        current: 2,
+        previous: 2,
       },
-      "monthly": {
-        "current": 7,
-        "previous": 11
-      }
-    }
-  }
+      monthly: {
+        current: 7,
+        previous: 11,
+      },
+    },
+  },
 ];
-
 
 const selector = document.querySelectorAll('a[href="javascript:void(0)"]');
 const cardsData = document.querySelectorAll(".data__stats");
 
-
 const insertValue = (value, index) => {
-  cardsData[index].querySelector("p").innerHTML = value.previous > 1 ? `Previous - ${value.previous}hrs`:`Previous - ${value.previous}hr`;
+  cardsData[index].querySelector("p").innerHTML =
+    value.previous > 1
+      ? `Previous - ${value.previous}hrs`
+      : `Previous - ${value.previous}hr`;
 
-  cardsData[index].querySelector("h2").innerHTML = value.current > 1 ? `${value.current}hrs` : `${value.current}hr`;
-}
+  cardsData[index].querySelector("h2").innerHTML =
+    value.current > 1 ? `${value.current}hrs` : `${value.current}hr`;
+};
 
-data.map((value, index)=> { // Carga datos iniciales daily por defecto
+// Carga datos iniciales daily por defecto
+data.map((value, index) => {
   insertValue(value.timeframes.daily, index);
 });
 
-
-for(let boton of selector){
+for (let boton of selector) {
   boton.addEventListener("click", () => {
     const actual = document.querySelector(".selected");
 
-    if(boton.id != actual.id){
-      boton.classList.toggle('selected');
-      actual.classList.toggle('selected');
+    if (boton.id != actual.id) {
+      boton.classList.toggle("selected");
+      actual.classList.toggle("selected");
       changeCards(boton.id);
     }
-  })
+  });
 }
 
 const changeCards = (option) => {
   console.log(option);
-  switch(option){
-    case 'daily':
-      data.map((value, index)=> {
-	insertValue(value.timeframes.daily, index);
+  switch (option) {
+    case "daily":
+      data.map((value, index) => {
+        insertValue(value.timeframes.daily, index);
       });
       break;
 
-    case 'weekly':
-      data.map((value, index)=> {
-	insertValue(value.timeframes.weekly, index);
+    case "weekly":
+      data.map((value, index) => {
+        insertValue(value.timeframes.weekly, index);
       });
       break;
 
-    case 'monthly':
-      data.map((value, index)=> {
-	insertValue(value.timeframes.monthly, index);
+    case "monthly":
+      data.map((value, index) => {
+        insertValue(value.timeframes.monthly, index);
       });
       break;
 
     default:
       console.log("Error at getting option");
   }
-}
-
-
+};
